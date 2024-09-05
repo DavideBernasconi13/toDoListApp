d5616381b165c7641e4fc70a7ffc4f39b397f4a1
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-09-05 15:33:55 +0200

    changeStatusTask, deleteTask and updateTask, showMenuTask

--- main.js
+++ main.js
@@ -1,20 +1,106 @@
 const taskInput = document.querySelector(".task-input input");
+const taskBox = document.querySelector(".tasks-box");
+
+
 //getting localeStorage
 let todos = JSON.parse(localStorage.getItem("todo-list"));
+if (!todos) { //if todos don't exist, pass an empty array
+    todos = [];
+}
 
-//mostra i todo
+/* -------------------------------------------------------------------------------- */
+/* ! Show to do */
+/* -------------------------------------------------------------------------------- */
 function showTodo() {
-    todos.forEach((todo, id) => {
-        console.log(id, todo);
+    let li = ""; //create variable
+    if (todos) {
+        todos.forEach((todo, id) => {
+            // se lo stato è completo metti il valore checked
+            let isCompleted = todo.status == "completed" ? "checked" : "";
+            //Object literals with html code 
+            li += `<li class="task">
+        <label for="${id}">
+                    <input onclick="updateStatus(this)" type="checkbox" id="${id}">
+                    <p class= ${isCompleted}>${todo.name}</p>
+                </label>
+                <div class="settings">
+                    <i onclick="showMenu(this)" class="fa fa-ellipsis-h" aria-hidden="true"></i>
+                    <ul class="task-menu">
+                        <li onclick="editTask(${id}, '${todo.name}')"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</li>
+                        <li onclick="deleteTask(${id})"><i class="fa fa-trash" aria-hidden="true"></i>Delete</li>
+                    </ul>
+                </div>
+                </li>`
+        });
+    }
+
+    taskBox.innerHTML = li;
+    if (todos.length === 0) {
+        taskBox.innerHTML = `<h2>Non hai ancora nessun task, generane uno</h2>`
+    }
+}
+
+showTodo();
+
+/* -------------------------------------------------------------------------------- */
+/* ! Show menu task */
+/* -------------------------------------------------------------------------------- */
+function showMenu(selectedTask) {
+    let taskMenu = selectedTask.parentElement.lastElementChild;
+    taskMenu.classList.add("show");
+    document.addEventListener("click", e => {
+        //remove class show when click 
+        if (e.target.tagName != "I" || e.target != selectedTask) {
+            taskMenu.classList.remove("show");
+        }
     })
 }
+/* -------------------------------------------------------------------------------- */
+/* ! Edit task */
+/* -------------------------------------------------------------------------------- */
+function editTask(taskId, taskName) {
+    console.log(taskId);
+    taskInput.value = taskName;
+}
+
+/* -------------------------------------------------------------------------------- */
+/* ! Delete task */
+/* -------------------------------------------------------------------------------- */
+function deleteTask(deleteid) {
+    //remove item from array todos
+    todos.splice(deleteid, 1);
+    //update localStorage
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    //aggiorna l'intefaccia
+    showTodo();
+}
+
+/* -------------------------------------------------------------------------------- */
+/* ! Update status */
+/* -------------------------------------------------------------------------------- */
+function updateStatus(selectedTask) {
+    let taskName = selectedTask.parentElement.lastElementChild;
+    if (selectedTask.checked) {
+        taskName.classList.add("checked");
+        //update satus to compled
+        todos[selectedTask.id].status = "completed";
+    } else {
+        taskName.classList.remove("checked");
+        // update status to pending
+        todos[selectedTask.id].status = "pending";
+    }
+    //update localStorage
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+}
+
+
+/* -------------------------------------------------------------------------------- */
+/* ! Insert task */
+/* -------------------------------------------------------------------------------- */
 
 taskInput.addEventListener("keyup", e => {
     let userTask = taskInput.value.trim();
     if (e.key == "Enter" && userTask) {
-        if (!todos) { //if todos don't exist, pass an empty array
-            todos = [];
-        }
         taskInput.value = ""; //pulisce il valore nell'input
         let taskInfo = { name: userTask, status: "pending" }; //crea il task 
         todos.push(taskInfo); // add a task
@@ -22,3 +108,4 @@ taskInput.addEventListener("keyup", e => {
         showTodo();
     }
 })
+
